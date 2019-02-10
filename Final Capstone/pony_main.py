eaee591e80ea5496fac685e7d23dd021687424db
tsfrench <35277611+tshaefrench@users.noreply.github.com>
2019-02-10 15:46:12 -0500

    Add files via upload

---
+++ Final Capstone/pony_main.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from pony_orm_model import *
+import csv
+
+@db_session
+def add_job(title, job_description):
+    d = Job()
+    d.title = title
+    commit()
+    d.job_description = job_description
+    commit()
+    d.job_class = job_class
+    commit()
+
+populate_database()
